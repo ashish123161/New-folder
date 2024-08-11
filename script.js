d61232b191973152ba5acf13e2f6f6c509847e80
ashish <ashishchaudhary@gmail,com>
2024-08-11 15:31:55 +0530

    index and javascript file add to new repo

---
+++ script.js
@@ -0,0 +1,131 @@
+
+// fullname = 'ashish chaudhary'
+// // age = 24;
+// reduis = 12;
+// a = null;
+// y = undefined;
+
+// console.log(y)
+
+
+// let fullName ="ashish chaudhry";
+// let age = 23;
+ 
+// console.log(fullName)
+
+
+// let a = 5;
+// let b = 8;
+
+// console.log("a =", a,"& b =", b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b =", a * b);
+// console.log("a / b =", a / b);
+// console.log("a % b =", a % b);
+// console.log("a ** b =", a ** b);
+// let a = 5;
+// let b = 2;
+
+// console.log("a = ", a," & b = ", b);
+// console.log("a-- = ", a--);
+// console.log("a = ", a);
+
+// let a = 5;
+// let b = 2;
+
+// a += 4;
+// console.log("a = ", a);
+
+// let a = 5;
+// let b = 2;
+
+// console.log("5 == 2", a == b);
+
+// let a = 6;
+// let b = 5;
+
+//  console.log("!(6<5) = ", !(a < b)  );
+
+
+// let age = 17;
+// let mode = "dark";
+// let color;
+
+// if(mode === "dark"){
+//     color = "black";
+// } else{
+//     color ="white";
+// }
+
+// if(mode === "light"){
+//     color = "red";
+// }
+// console.log(color);
+
+// if (age >= 18) {
+//     console.log("you can vote");
+// }
+
+
+// if (age < 16){
+//     console.log("you can not vote");
+// }
+
+// if (age = 17){
+//     console.log("you are wait one year");
+// }
+
+
+// ternary operator
+
+// age = 15;
+
+// age >=23 ?console.log("adult") : console.log("not adult");
+
+// const greeting = (person) => {
+//     const name = person ? person.name : "stranger";
+//     return `Howdy, ${name}`;
+//   };
+  
+//   console.log(greeting({ name: "Ashis" })); 
+//   console.log(greeting(null)); 
+// let num = prompt("enter a number:");
+
+
+// if (num % 5  === 0) {
+//     console.log(num, "your number is multiple of 5 ");
+
+// } else {
+//     console.log(num, "your number is not  multiple of 5 ");
+// }
+
+
+// let score = prompt("enter your number :(0 - 100)");
+// let grade;
+
+// if (score >=90 && score <=100){
+//     grade = "A";
+// }else if (score >=70 && score <=89){
+//     grade ="B";
+// }else if (score >=60 && score <= 69){
+//     grade = "C";
+// }else if (score >=50 && score <=59){
+//     grade = "D";
+// }else if (score >=0 && score <=49){
+//     grade ="F";
+// }
+
+// console.log("according to your score, your grade was : " , grade);
+
+
+
+
+// let sum = 0;
+// let n = 5;
+
+// for(i = 1; i <= n; i++){
+//     sum = sum + i;
+// }
+// console.log("sum =" , sum);
+// console.log("loop has end ");
